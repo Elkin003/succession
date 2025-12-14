e5611b954b29d8b0e16291c2604a56b7e6ff3599
Ego_Code <criss.2004.guaman@gmail.com>
2025-12-14 10:39:08 -0500

    Implementación de la serie 4 y 5 ya funcionales

--- src/main/java/edu/unl/cc/Main.java
+++ src/main/java/edu/unl/cc/Main.java
@@ -1,8 +1,6 @@
 package edu.unl.cc;
 
-import edu.unl.cc.succession.business.EvenNumberCalculatorUpToLimit;
-import edu.unl.cc.succession.business.PrimeNumberCalculatorUpToLimit;
-import edu.unl.cc.succession.business.PrimeNumberWithPowCalculatorWithTerm;
+import edu.unl.cc.succession.business.*;
 import edu.unl.cc.succession.domain.Successionable;
 
 import java.util.Scanner;
@@ -16,6 +14,8 @@ public class Main {
         System.out.println("CALCULADORA DE SERIES");
         System.out.println("1. Serie de números pares hasta un limite (S = 2 + 4 + 6 + 8 + ... N): ");
         System.out.println("2. Serie de primos elevados al cubo  hasta un limite (S = 1^3 + 3^3 + 5^3 + 7^3 + 11^3 + 13^3 ... + N^3): ");
+        System.out.println("4. Serie de primos elevados por pares hasta un limite (S = 1^2 + 3^4 + 5^6 + 7^8 + 11^10 + 13^(12) ... + N): ");
+        System.out.println("5.  Serie de primos elevados a impares hasta n térmimos (S = S = 1^1 + 3^3 + 5^5 + 7^7 + 11^9 + 13^11 ..): ");
         System.out.println("7. Serie de primos elevados a la raiz de numeros impares hasta un n términos (S = 1^(1/1) + 3^(1/3) + 5^(1/5) + 7^(1/7) + 11^(1/9) + 13^(1/11)): ");
         System.out.println("10. Serie de primos hasta un limite (S = 1 + 2 + 3 + 5 + 7 + 11 + 13 + .. + N: ");
     }
@@ -44,7 +44,12 @@ public class Main {
             case 2:
                 //ToDO
                 break;
-
+            case 4:
+                successionable = new EvenExponentPrimeNumberCalculatorUpToLimit(limit);
+                break;
+            case 5:
+                successionable = new OddExponentPrimeNumberCalculatorUpToNTerms(limit);
+                break;
 
             case 7:
                 successionable = new PrimeNumberWithPowCalculatorWithTerm(limit);
--- src/main/java/edu/unl/cc/succession/business/OddExponentPrimeNumberCalculatorUpToNTerms.java
+++ src/main/java/edu/unl/cc/succession/business/OddExponentPrimeNumberCalculatorUpToNTerms.java
@@ -1,4 +1,76 @@
 package edu.unl.cc.succession.business;
+import edu.unl.cc.succession.domain.Successionable;
+/**
+ * Representa el cálculo de la Serie de primos elevados a impares hasta n térmimos
+ * (S = S = 1^1 + 3^3 + 5^5 + 7^7 + 11^9 + 13^11 ..):
+ * @author Cristian Guaman
+ */
+public class OddExponentPrimeNumberCalculatorUpToNTerms implements Successionable {
+    private Integer limit;
+    private Integer currentTerm;
+    private StringBuilder printableTerms;
 
-public class OddExponentPrimeNumberCalculatorUpToNTerms {
+    public OddExponentPrimeNumberCalculatorUpToNTerms(Number limit) {
+        this(0, limit);
+    }
+
+    public OddExponentPrimeNumberCalculatorUpToNTerms(Number start, Number limit) {
+        if (start.intValue() < 0){
+            throw new IllegalArgumentException("Start must be greater than 0");
+        }
+        setLimit(limit);
+        currentTerm = nextTerm(start).intValue();
+        this.printableTerms = new StringBuilder("S = ");
+    }
+
+    private boolean isPrime(Integer number){
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    public void setLimit(Number limit) {
+        if (limit.intValue() < 0){
+            throw new IllegalArgumentException("Limit must be greater than 0");
+        }
+        this.limit = limit.intValue();
+    }
+
+    @Override
+    public Number nextTerm(Number currentTerm) {
+        currentTerm = currentTerm.intValue() + 1;
+        boolean isPrime = false;
+        while (!isPrime){
+            isPrime = isPrime(currentTerm.intValue());
+            if (!isPrime){
+                currentTerm = currentTerm.intValue() + 1;
+            }
+        }
+        return currentTerm;
+    }
+
+    @Override
+    public Number calculate() {
+            long     result = 0;
+            int counterTerm = 0;
+            int exponent = 1;
+            int currentTerm = this.currentTerm > 0 ? this.currentTerm -1 : 0;
+            while (counterTerm < limit){
+                currentTerm = nextTerm(currentTerm).intValue();
+                this.printableTerms.append(currentTerm).append("^")
+                        .append(exponent).append(" + ");
+                result = (long) (result + Math.pow(currentTerm,exponent));
+                counterTerm++;
+                exponent+=2;
+            }
+            return result;
+        }
+
+    @Override
+    public String print() {
+        return printableTerms.toString();
+    }
 }
