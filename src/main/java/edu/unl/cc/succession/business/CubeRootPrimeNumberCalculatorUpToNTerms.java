1cc34aa76c0f197d4207087930ca033bfee7bb23
KanekiKen131 <alexanderlimaabad@gmail.com>
2025-12-14 17:41:03 -0500

    Solucion de los ejercicios 6 y 8

--- src/main/java/edu/unl/cc/succession/business/CubeRootPrimeNumberCalculatorUpToNTerms.java
+++ src/main/java/edu/unl/cc/succession/business/CubeRootPrimeNumberCalculatorUpToNTerms.java
@@ -1,4 +1,82 @@
 package edu.unl.cc.succession.business;
 
-public class CubeRootPrimeNumberCalculatorUpToNTerms {
+import edu.unl.cc.succession.domain.Successionable;
+
+/**
+ * Serie de primos elevados a la raiz cúbica hasta un N términos
+ * (S = 1^(1/3) + 3^(1/3) + 5^(1/3) + 7^(1/3) + 11^(1/3) + 13^(1/3)):
+ * @author Leonel Lima (LMess)
+ */
+
+public class CubeRootPrimeNumberCalculatorUpToNTerms implements Successionable {
+
+    private Integer currentTerm;
+    private Integer limit;
+    private StringBuilder printableTerms;
+
+    public CubeRootPrimeNumberCalculatorUpToNTerms (Number limit) {
+        this(0, limit);
+    }
+
+    public CubeRootPrimeNumberCalculatorUpToNTerms(Number start, Number limit) {
+        if (start.intValue() < 0){
+            throw new IllegalArgumentException("Start must be greater than 0");
+        }
+        setLimit(limit);
+        this.currentTerm = start.intValue();
+        this.printableTerms = new StringBuilder("S = ");
+    }
+
+    private Boolean isPrime(Integer number){
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2 ; i < number; i++){
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void setLimit(Number limit) {
+        if (limit.intValue() < 0){
+            throw new IllegalArgumentException("Limit must be greater than 0");
+        }
+        this.limit = limit.intValue();
+    }
+
+    @Override
+    public Number nextTerm(Number currentTerm) {
+        currentTerm = currentTerm.intValue() + 1;
+        while (!isPrime(currentTerm.intValue())){
+            currentTerm = currentTerm.intValue() + 1;
+        }
+        return currentTerm;
+    }
+
+    @Override
+    public Number calculate() {
+        double result = 0.0;
+        int counterTerm = 0;
+        int currentTerm = this.currentTerm > 0 ? this.currentTerm : 2;
+        while (counterTerm < limit) {
+            currentTerm = nextTerm(currentTerm).intValue();
+            this.printableTerms.append(currentTerm).append("^(1/3)");
+            if (counterTerm < limit - 1) {
+                this.printableTerms.append(" + ");
+            }
+            result += Math.pow(currentTerm, 1.0 / 3);
+            counterTerm++;
+        }
+        return result;
+    }
+
+
+    @Override
+    public String print() {
+        return printableTerms.toString();
+    }
+
 }
--- src/main/java/edu/unl/cc/succession/business/EvenRootPrimeNumberCalculatorUpToLimit.java
+++ src/main/java/edu/unl/cc/succession/business/EvenRootPrimeNumberCalculatorUpToLimit.java
@@ -1,4 +1,83 @@
 package edu.unl.cc.succession.business;
 
-public class EvenRootPrimeNumberCalculatorUpToLimit {
+import edu.unl.cc.succession.domain.Successionable;
+
+/**
+ * Serie de primos elevados a la raiz de numeros pares hasta un limite
+ * (S = 1^(1/2) + 3^(1/4) + 5^(1/6) + 7^(1/8) + 11^(1/10) + 13^(1/12) ... + N):
+ * @author Leonel Lima (LMess)
+ */
+public class EvenRootPrimeNumberCalculatorUpToLimit implements Successionable {
+
+    private Integer currentTerm;
+    private Integer limit;
+    private StringBuilder printableTerms;
+
+    public EvenRootPrimeNumberCalculatorUpToLimit(Number limit) {
+        this(0,limit);
+    }
+
+    public EvenRootPrimeNumberCalculatorUpToLimit(Number start, Number limit) {
+        if (start.intValue() < 0) {
+            throw new IllegalArgumentException("Start must be greater than 0");
+        }
+        setLimit(limit);
+        this.currentTerm = start.intValue();
+        this.printableTerms = new StringBuilder("S = ");
+    }
+
+    private Boolean isPrime(Integer number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void setLimit(Number limit) {
+        if (limit.intValue() < 0){
+            throw new IllegalArgumentException("Limit must be greater than 0");
+        }
+        this.limit = limit.intValue();
+    }
+
+    @Override
+    public Number nextTerm(Number currentTerm) {
+        currentTerm = currentTerm.intValue() + 1;
+        while (!isPrime(currentTerm.intValue())){
+            currentTerm = currentTerm.intValue() + 1;
+        }
+        return currentTerm;
+    }
+
+    @Override
+    public Number calculate() {
+        double result = 0.0;
+        int counterTerm = 0;
+        int currentTerm = this.currentTerm > 0 ? this.currentTerm : 2;
+
+        while (counterTerm < limit) {
+            currentTerm = nextTerm(currentTerm).intValue();
+            int evenRootExponent = (counterTerm * 2) + 2;
+            this.printableTerms.append(currentTerm).append("^(1/").append(evenRootExponent).append(")");
+            if (counterTerm < limit - 1) {
+                this.printableTerms.append(" + ");
+            }
+            result += Math.pow(currentTerm, 1.0 / evenRootExponent);
+            counterTerm++;
+        }
+        return result;
+    }
+
+
+    @Override
+    public String print() {
+        return printableTerms.toString();
+    }
+
 }
